03dc4e6a6c381b33fdef62fc03a748090bb67f7e
Dor Genosar <13957883+dorg188@users.noreply.github.com>
2023-06-16 16:08:46 +0300

    Added setup.py classifiers

--- setup.py
+++ setup.py
@@ -11,5 +11,15 @@ setup(
     license_files=['LICENSE'],
     packages=['requestsbuilder'],
     install_requires=['requests'],
-    keywords=[]
+    python_requires='>=3.7',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities'
+    ]
 )
