641f75030ec8547f1c6b64df696ee2b648a1fa39
Dor Genosar <13957883+dorg188@users.noreply.github.com>
2023-06-16 15:44:03 +0300

    Added setup.py and reordered files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='requests-builder',
+    version='1.0.0',
+    description='A cool utility to save and reuse requests with the requests library',
+    author='Labrador Team',
+    author_email='labrador.team@outlook.com',
+    license='Apache 2.0',
+    license_files=['LICENSE'],
+    packages=['requestsbuilder'],
+    install_requires=['requests'],
+    keywords=[]
+)
